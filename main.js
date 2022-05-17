c8ddf60fb067eebdd0bae8f42909f5754cd08e47
Zoli <sapi.matejka.zoltan@hmei.hu>
2022-05-17 21:24:30 +0200

    new

--- main.js
+++ main.js
@@ -8,3 +8,29 @@ let userAge = 33;
 /*Nem lehet megváltoztatni*/
 const age = 44;
 const valami = "Más valami"
+/*változók típusai*/
+let num = 4;
+num.toString()
+"4"
+name = "almafa";
+"almafa"
+name.replace("a", "b")
+"blamafa"
+/*számok*/
+let userAge = 33;
+userAge +5
+38
+6 % 3 
+0 /*maradékot írja ki*/
+/*hatványozás*/ 3**2
+var num =1;
+num + 1
+2
+num - 1
+0
+num = num + 5;
+6
+num / 2
+3
+num * 2
+12
