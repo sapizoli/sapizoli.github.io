4f96a19cf990b7e5b0094def115d58212049cdd9
Sápi-Matejka Zoltán <sapi.matejka.zoltan@iad.intranet.local>
2022-05-17 13:58:28 +0200

    new

---
+++ main.js
@@ -0,0 +1,10 @@
+console.log("Helo");
+/*meg lehet változtatni*/
+let price = 5;
+let price2 = 6;
+let total = price1+price2;
+let name;
+let userAge = 33;
+/*Nem lehet megváltoztatni*/
+const age = 44;
+const valami = "Más valami"
