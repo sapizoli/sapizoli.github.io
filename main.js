ce9a8ccd7a0a2a61e64f3f033e9ee732d21c6aaf
Sápi-Matejka Zoltán <sapi.matejka.zoltan@iad.intranet.local>
2022-05-18 06:37:45 +0200

    je

--- main.js
+++ main.js
@@ -7,4 +7,35 @@ let name;
 let userAge = 33;
 /*Nem lehet megváltoztatni*/
 const age = 44;
-const valami = "Más valami"
+const valami = "Más valami";
+/*típusok*/
+let num = 4;
+boolean, number, string, null, undefined
+typeof num
+let active = true;
+typeof active
+name = "almafa";
+"almafa"
+name.replace("a", "b")
+"blamafa"
+num.toString /*dotoperátor*/
+"4"
+/*számok*/
+let userAge = 33;
+userAge +5
+38
+6 % 3 
+0 /*maradékot írja ki*/
+/*hatványozás*/ 3**2
+let num =1;
+num + 1
+2
+num - 1
+0
+num = num + 5;
+6
+num / 2
+3
+num * 2
+12
+
