9657ef907021c881f3bbd32ccd06e6fc16463733
Sápi-Matejka Zoltán <sapi.matejka.zoltan@iad.intranet.local>
2022-05-19 13:53:38 +0200

    new

---
+++ tomb.js
@@ -0,0 +1,33 @@
+/*let cars = ["Ford", "Audi", "Volvo"];
+cars
+(3) ["Ford", "Audi", "Volvo"]
+let user = [1, "Joe", true, null];
+user[2]
+true
+let users = [
+    [1, "Joe", true],
+    [2, "Jim", false],
+];
+Array.isArray(users)
+true
+let test2 = "helo";
+Array.isArray(test2)
+false
+
+tömbök metódusai (arr.oush();, arr.pop());arr. unshit(); arr.shit();
+let cars = ["Ford", "Audi", "Volvo"];
+cars.push("trabant"); -->Hozzáadja a végéhez
+4
+cars
+(4) ["Ford", "Audi", "Volvo", "Trabant"]
+cars.pop() -->Kiveszi a tömb végéből
+"Trabant"
+(3) ["Ford", "Audi", "Volvo"]
+cars.unshift("Lada"); --> a tömb elejére adja hozzá
+4
+cars
+(4) ["Lada", Ford", "Audi", "Volvo"]
+cars.shift(); --> a tömb elejét veszi el
+"Lada"
+cars
+(3) ["Ford", "Audi", "Volvo"]
