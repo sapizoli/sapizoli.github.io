ac3e0ba98f47f12b5380b36623450eb88c715b26
Sápi-Matejka Zoltán <sapi.matejka.zoltan@iad.intranet.local>
2022-05-19 12:54:02 +0200

    new

--- scope.js
+++ scope.js
@@ -8,3 +8,21 @@ function something(){
 something();
 
 console.log(globalName);
+
+//let
+let name = "Sanyi";
+{
+    let name = "Pisti";
+    console.log(name);
+}
+console.log(name);
+
+//const
+const NAME = "Sanyi";
+
+    function test(){
+    const name = "Pisti";
+    console.log(NAME);
+}
+test();
+console.log(NAME);
