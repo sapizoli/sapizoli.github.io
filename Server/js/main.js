cd2fd63114064b228e5fa6909a9950d5a787f3ae
Sápi-Matejka Zoltán <sapi.matejka.zoltan@iad.intranet.local>
2022-05-30 11:36:30 +0200

    new

--- Server/js/main.js
+++ Server/js/main.js
@@ -17,13 +17,62 @@ document.querySelector("#getDataBtn").addEventListener("click", function () {
 });
 //Fill table with server data.
 function fillDataTable(data, tableID) {
-    let table = document.querySelector('#${tableID}');
+    let table = document.querySelector(#${tableID});
     if (!table) {
-        console.error('Table "${tableID}" is not found.');
+        console.error('Table "${tableID}" is not found');
         return;
     }
     let tBody = table.querySelector("tbody");
     for (let row of data) {
-        console.log(row);
+        let tr = createAnyElement("tr");
+        for (let k in row) {
+            let td = createAnyElement("td");
+            td.innerHTML = row[k];
+            tr.appendChild(td);
+        }
+        let btnGroup = createBtnGroup();
+        tr.appendChild(btnGroup);
+        tBody.appendChild(tr);
     }
 }
+
+function createAnyElement(name, attributes) {
+    let element = document.createElement(name);
+    for (let k in attributes) {
+        element.setAttribute(k, attributes[k]);
+    }
+    return element;
+}
+
+function createBtnGroup() {
+    let group = createAnyElement("div", { class: "btn btn - group" });
+    let infoBtn = createAnyElement("button", { class: "btn btn-info", onclick:"getInfo(this)" });
+    infoBtn.innerHTML = '<i class="fa fa-refresh" aria-hidden="true"></i>';
+    let delBtn = createAnyElement("button", { class: "btn btn-danger", onclick:"delRow(this)" });
+    infoBtn.innerHTML = '<i class="fa fa-trash" aria-hidden="true"></i>';
+
+    group.appendChild(infoBtn);
+    group.appendChild(delBtn);
+
+    let td = createAnyElement("td");
+    td.appendChild(group);
+    return td;
+}
+
+//Delete metódus
+function delRow(btn){
+    let tr = btn.paretElement.paretElement.paretElement;
+    let id = tr.querySelector("td: first-child").innerHTML;
+    let fetchOptions = {
+        method: "DELETE",
+        mode: "cors",
+        cache: "no-cache"
+    };
+    fetch ('http://locakhost:3000/users/${id}', fetchOptions).then
+    resp => resp.json()
+    err => console.error(err)
+}. then(
+    data => {
+
+    }
+);
