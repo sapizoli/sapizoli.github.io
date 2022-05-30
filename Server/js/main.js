84a1193f3ecbc2019dc2914bce565f30c4d511ed
Sápi-Matejka Zoltán <sapi.matejka.zoltan@iad.intranet.local>
2022-05-30 09:28:17 +0200

    new

--- Server/js/main.js
+++ Server/js/main.js
@@ -1,17 +1,29 @@
 //GET data from the server
-function getServerData(url){
+function getServerData(url) {
     let fetchOptions = {
         method: "GET",
         mode: "cors",
         cache: "no-cache"
     };
-    fetch(url, fetchOptions).then(
+    return fetch(url, fetchOptions).then(
         response => response.json(),
         err => console.error(err)
-    ).then(
-        data => console.log(data)
     );
 }
-getServerData("http://localhost:3000/users").then(
-    data => console.log(data)
-);
+document.querySelector("#getDataBtn").addEventListener("click", function () {
+    getServerData("http://localhost:3000/users").then(
+        data => fillDataTable(data, "userTable")
+    );
+});
+//Fill table with server data.
+function fillDataTable(data, tableID) {
+    let table = document.querySelector('#${tableID}');
+    if (!table) {
+        console.error('Table "${tableID}" is not found.');
+        return;
+    }
+    let tBody = table.querySelector("tbody");
+    for (let row of data) {
+        console.log(row);
+    }
+}
